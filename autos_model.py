c7e191773e8be0fbf8848335922c7c55582c3050
BillySh <A01657143@tec.mx>
2023-11-10 19:12:05 -0600

    Finalización de modelos

--- autos_model.py
+++ autos_model.py
@@ -33,6 +33,42 @@ mapaObs = [(0,0),(1,0),(2,0),(3,0),(0,1),(1,1),(2,1),(3,1),
             (19,11),(18,11),(17,11),(16,11), (19,12),(18,12),(17,12),(16,12), (19,13),(18,13),(17,13),(16,13),
             (19,19),(18,19),(17,19),(16,19), (19,18),(18,18),(17,18),(16,18)
            ]
+mapaIz = [(4,0), (5,0), (4,1), (5,1),(5,2),(4,2) ,(4,3), (5,3), (4,4), (5,4), (4,5), (5,5), (4,6), (5,6),
+          (4,7), (5,7), (4,8), (5,8), (4,9), (5,9), (4,10), (5,10), (4,11), (5,11), (4,12), (5,12),
+          (4,13), (5,13), (4,14), (5,14), (4,15), (5,15), (4,16), (5,16), (4,17), (5,17), 
+          (4,18), (5,18), (4,19), (5,19),
+          #Primera sección carril izquierda 4,5 X
+          (12,0),(13,0),(12,1),(13,1),(12,2),(13,2),(12,3),(13,3),(12,4),(13,4),(12,5),(13,5),(12,6),(13,6),
+          (12,7),(13,7),(12,8),(13,8),(12,9),(13,9),(12,10),(13,10),(12,11),(13,11),(12,12),(13,12),(12,13),(13,13),(12,14),(13,14),
+          (12,15),(13,15),(12,16),(13,16),(12,17),(13,17),(12,18),(13,18),(12,19),(13,19),
+          #Segunda columna carril izquierda 12,13 X
+          (0,8),(0,7),(1,8),(1,7),(2,8),(2,7),(3,8),(3,7),(4,8),(4,7),(5,8),(5,7),(6,8),(6,7),(7,8),(7,7),(8,8),(8,7),(9,8),(9,7),
+          (10,8),(10,7),(11,8),(11,7),(12,8),(12,7),(13,8),(13,7),(14,8),(14,7),(15,8),(15,7),(16,8),(16,7),(17,8),(17,7),(18,8),(18,7),
+          (19,8),(19,7),
+          #Primer columna carril izquierda Y 8,7
+          (0,16),(0,17),(1,16),(1,17),(2,16),(2,17),(3,16),(3,17),(4,16),(4,17),(5,16),(5,17),(6,16),(6,17),(7,16),(7,17),(8,16),(8,17),(9,16),(9,17),
+          (10,16),(10,17),(11,16),(11,17),(12,16),(12,17),(13,16),(13,17),(14,16),(14,17),(15,16),(15,17),(16,16),(16,17),(17,16),(17,17),(18,16),(18,17),
+          (19,16),(19,17)
+          #Segunda coluna del carril izquierdo Y 17,16
+
+           ]
+mapaDer = [#Primera fila del carril derecha 6,7 X
+    (6,0),(7,0),(6,1),(7,1),(6,2),(7,2),(6,3),(7,3),(6,4),(7,4),(6,5),(7,5),(6,6),(7,6),
+    (6,7),(7,7),(6,8),(7,8),(6,9),(7,9),(6,10),(7,10),(6,11),(7,11),(6,12),(7,12),(6,13),(7,13),
+    (6,14),(7,14),(6,15),(7,15),(6,16),(7,16),(6,17),(7,17),(6,18),(7,18),(6,19),(7,19),
+    #Segunda fila del carril de la derecha 14,15 X
+    (14,0),(15,0),(14,1),(15,1),(14,2),(15,2),(14,3),(15,3),(14,4),(15,4),(14,5),(15,5),(14,6),(15,6),
+    (14,7),(15,7),(14,8),(15,8),(14,9),(15,9),(14,10),(15,10),(14,11),(15,11),(14,12),(15,12),(14,13),(15,13),
+    (14,14),(15,14),(14,15),(15,15),(14,16),(15,16),(14,17),(15,17),(14,18),(15,18),(14,19),(15,19),
+    #Primera fila del carril de la derecha 5,6 Y
+    (0,5),(0,6),(1,5),(1,6),(2,5),(2,6),(3,5),(3,6),(4,5),(4,6),(5,5),(5,6),(6,5),(6,6),(7,5),(7,6),(8,5),(8,6),
+    (9,5),(9,6),(10,5),(10,6),(11,5),(11,6),(12,5),(12,6),(13,5),(13,6),(14,5),(14,6),(15,5),(15,6),(16,5),(16,6),
+    (17,5),(17,6),(18,5),(18,6),(19,5),(19,6),
+    #Segunda fila del carril de la derecha 14,15 Y
+    (0,14),(0,15),(1,14),(1,15),(2,14),(2,15),(3,14),(3,15),(4,14),(4,15),(5,14),(5,15),(6,14),(6,15),(7,14),(7,15),(8,14),(8,15),
+    (9,14),(9,15),(10,14),(10,15),(11,14),(11,15),(12,14),(12,15),(13,14),(13,15),(14,14),(14,15),(15,14),(15,15),(16,14),(16,15),
+    (17,14),(17,15),(18,14),(18,15),(19,14),(19,15)
+           ]
 #---------------------------Agents----------------------------------------
 
 """
@@ -48,7 +84,7 @@ class CarAgent(Agent): #Car
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.speed = 0  # Initial speed
-        self.locationX = 5  # Initial location
+        self.locationX = 6  # Initial location
         self.locationY = 0  # Initial location
         self.agentT = 2 #DrunkDriver 
 
@@ -67,8 +103,8 @@ class CarAgent(Agent): #Car
             #print("Modulo de 9:", (self.locationY +1) %10)
         if self.locationX >=19 and self.locationY >=19:
             possible = (((self.locationX + 1)%20, self.locationY),((self.locationX - 1)%20, self.locationY), (self.locationX , (self.locationY +1)%20), ((self.locationX+1)%20, (self.locationY+1)%20))
-        print("posible_New:", possible)
-        print(possible_steps)
+        #print("posible_New:", possible)
+        #print(possible_steps)
         for neighbor_cell in possible:
             #print("Neighbor:",neighbor_cell)
             cell_contents = neighbor_cell
@@ -76,16 +112,16 @@ class CarAgent(Agent): #Car
             #print("Cellmate:", cellmates)
             if len(cellmates) >=1: 
                 other = cellmates[0]
-                print("Celmates[0]:", other)
+                #print("Celmates[0]:", other)
                 if other.agentT == 0:
                     value_to_remove = cell_contents
                     possible = tuple(value for value in possible if value != value_to_remove)
-                    print("posible_RemoveDrunk:", possible)
+                    #print("posible_RemoveDrunk:", possible)
                 #print("Entró")
 
         new_position = self.random.choice(possible)
         self.model.grid.move_agent(self, new_position)
-        print("new positionDrunk:", new_position)
+        #print("new positionDrunk:", new_position)
         self.locationX, self.locationY = new_position
 
     def step(self):
@@ -112,6 +148,15 @@ class carrilDerechoAgent(Agent):
     def step(self):
         pass
 
+class carrilizquierdoAgent(Agent):
+    def __init__(self,unique_id,model):
+        super().__init__(unique_id,model)
+        self.agentT = 4 #Obstacle
+     
+    
+    def step(self):
+        pass
+
 class CoolAgent(Agent):
     """
     Moving upward car agent
@@ -120,8 +165,7 @@ class CoolAgent(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.crashed = False
-        self.locationX = 4  # Initial location
-        self.locationY = 8
+        self.locationX, self.locationY = self.random.choice(mapaDer)
         self.agentT = 1 #SmoothOperator 
     
     def move(self):
@@ -129,14 +173,14 @@ class CoolAgent(Agent):
             self.pos, moore=True, include_center=False
         )
         
-        possible = ( (self.locationX , self.locationY +1), (self.locationX-1, self.locationY+1),(self.locationX-1, self.locationY))
-
+        possible = ( (self.locationX , self.locationY +1),(self.locationX+1, self.locationY))
+        #print("LocationX", self.locationX)
         if self.locationY >= 19:
-            possible = ( (self.locationX , (self.locationY +1) %20), (self.locationX-1, (self.locationY +1) %20),(self.locationX-1, self.locationY))
+            possible = ( (self.locationX , (self.locationY +1) %20), (self.locationX+1, self.locationY))
             #print("Above", possible)
             #print("Modulo de 9:", (self.locationY +1) %10)
-        if self.locationX <= -19:
-            possible = ( (self.locationX , self.locationY +1), ((self.locationX-1)%20, self.locationY +1),((self.locationX-1) %20, self.locationY))
+        if self.locationX >= 19:
+            possible = ( (self.locationX , self.locationY +1),((self.locationX+1)%20, self.locationY))
             #print("Above", possible)
             #print("Modulo de 9:", (self.locationY +1) %10)
         #print("posible_NewOtherCool:", possible)
@@ -149,12 +193,37 @@ class CoolAgent(Agent):
             #print("Cellmate:", cellmates)
             if len(cellmates) >=1: 
                 other = cellmates[0]
-                print("Celmates[0]:", other)
+                
+                print("Celmates:", cellmates)
                 if other.agentT == 0:
+                    #print("0:")
                     value_to_remove = cell_contents
                     possible = tuple(value for value in possible if value != value_to_remove)
-                    print("posible_RemoveDrunk:", possible)
+                    #print("posible_RemoveCool0:", possible)
+                
+                if other.agentT == 4:
+                    #print("4:")
+                    value_to_remove = cell_contents
+                    possible = tuple(value for value in possible if value != value_to_remove)
+                    #print("posible_RemoveCool4:", possible)
 
+            if len(cellmates) >=2:
+                print("Habemos muchos3")
+                other2 =  cellmates[1]
+                if other2.agentT == 2:
+                    print("Choque!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111111")
+                if other2.agentT == 1:
+                    print("Choque!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111111")
+            if len(cellmates) >=3:
+                print("Habemos muchos3")
+                other2 =  cellmates[2]
+                if other2.agentT == 2:
+                    print("Choque!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111111")
+                if other2.agentT == 1:
+                    print("Choque!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1111111")
+                    
+
+        #If possible <= 1: Move to same position
         new_position = self.random.choice(possible)
         self.model.grid.move_agent(self, new_position)
         print("new positionCool:", new_position)
@@ -175,13 +244,7 @@ class CarModel(Model):
         self.schedule = RandomActivation(self)
         o = 0
 
-        # Create car agents and add them to the schedule
-        for i in range(self.num_agents):
-            print("Auto")
-            car_agent = CarAgent(i, self)
-            self.schedule.add(car_agent)
-            self.grid.place_agent(car_agent, (car_agent.locationX, car_agent.locationY))
-            o +=1 #Contador de ids
+       
         
         #Crete the obstacles
         for i in mapaObs:
@@ -191,6 +254,23 @@ class CarModel(Model):
             self.schedule.add(pavA)
             self.grid.place_agent(pavA,(x,y))
             o += 1
+        #Create carril derecho
+        for i in mapaDer:
+            #print("Mapa")
+            pavA = carrilDerechoAgent(o,self)
+            x, y = i
+            self.schedule.add(pavA)
+            self.grid.place_agent(pavA,(x,y))
+            o += 1
+        #Create carril izquierdo
+        for i in mapaIz:
+            #print("Mapa")
+            pavA = carrilizquierdoAgent(o,self)
+            x, y = i
+            self.schedule.add(pavA)
+            self.grid.place_agent(pavA,(x,y))
+            o += 1
+        
 
         # Create car agents and add them to the schedule
         for i in range(self.num_agents):
@@ -198,6 +278,14 @@ class CarModel(Model):
             cool_agent = CoolAgent(o, self)
             self.schedule.add(cool_agent)
             self.grid.place_agent(cool_agent, (cool_agent.locationX, cool_agent.locationY))
+            o +=1 #Contador de ids
+        
+        # Create car agents and add them to the schedule
+        for i in range(self.num_agents):
+            print("Auto")
+            car_agent = CarAgent(o, self)
+            self.schedule.add(car_agent)
+            self.grid.place_agent(car_agent, (car_agent.locationX, car_agent.locationY))
             o +=1 #Contador de ids
 
     def step(self):
